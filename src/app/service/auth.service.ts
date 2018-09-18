fdc841180f25738655b60ce08f5f9beade36c0ea
kelum <edirisingheemks@gmail.com>
2018-09-18 16:57:40 +0530

    updated

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -19,13 +19,13 @@ export class AuthService {
   formData.append('lastname',user.lastname);
   formData.append('email',user.email);
   formData.append('phoneno',user.phoneno);
-  return this.http.post("http://localhost:9000/user/register", formData).map(res=>res.json()); 
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/register", formData).map(res=>res.json()); 
   };
 
   loginUser(user){
     let headers = new Headers();
     headers.append('content-Type','application/json');
-    return this.http.post("http://localhost:9000/user/login",user,{headers:headers}).map(res=>res.json());
+    return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/login",user,{headers:headers}).map(res=>res.json());
   };
 
   storeData(token,userdata){
@@ -40,7 +40,7 @@ getprofile(){
   let headers = new Headers();
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.get("http://localhost:9000/user/profile",{headers:headers}).map(res=>res.json());
+  return this.http.get("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/profile",{headers:headers}).map(res=>res.json());
   
 };
 fetchtoken(){
@@ -58,7 +58,7 @@ logOut(){
   this.authtoken = null;
   this.user = null;
   localStorage.clear();
-  return this.http.get("http://localhost:9000/user/logout",{headers:headers}).map(res=>res.json()); 
+  return this.http.get("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/logout",{headers:headers}).map(res=>res.json()); 
 }
 
 
@@ -71,7 +71,7 @@ testing(user){
   const formData: FormData = new FormData();
   formData.append('profpic', user.fileToUpload,user.fileToUpload.name);
   formData.append('fullname',user.fullname);
-  return this.http.post("http://localhost:9000/foodrecipe/c", formData).map(res=>res.json()); 
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/c", formData).map(res=>res.json()); 
 }
 
 addrecipe(newrecipe){
@@ -94,31 +94,31 @@ addrecipe(newrecipe){
   formData.append('rate',newrecipe.rate);
   formData.append('catagory',newrecipe.catagory);
   formData.append('description',newrecipe.description);
-  return this.http.post("http://localhost:9000/foodrecipe/addrecipe", formData).map(res=>res.json()); 
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/addrecipe", formData).map(res=>res.json()); 
 };
 
 getAllacceptedRecipe(){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/getallacceptedrecipe",{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/getallacceptedrecipe",{headers:headers}).map(res=>res.json());
 }
 
 getAllRecipe(){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/getallrecipe",{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/getallrecipe",{headers:headers}).map(res=>res.json());
 }
 
 getMostliked(){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/getmostliked",{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/getmostliked",{headers:headers}).map(res=>res.json());
 }
 
 getviewRecipe(recipename){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/getviewrecipe",recipename,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/getviewrecipe",recipename,{headers:headers}).map(res=>res.json());
 }
 
 likeRecipe(recipename){
@@ -130,7 +130,7 @@ likeRecipe(recipename){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/likerecipe",recipeData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/likerecipe",recipeData,{headers:headers}).map(res=>res.json());
 }
 
 unlikeRecipe(recipename){
@@ -142,7 +142,7 @@ unlikeRecipe(recipename){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/unlikerecipe",recipeData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/unlikerecipe",recipeData,{headers:headers}).map(res=>res.json());
 }
 
 checklike(recipename){
@@ -154,7 +154,7 @@ checklike(recipename){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/checklike",recipeData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/checklike",recipeData,{headers:headers}).map(res=>res.json());
 }
 
 getuserrecipes(myusername){
@@ -162,7 +162,7 @@ getuserrecipes(myusername){
   this.fetchtoken();
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/getuserrecipies",myusername,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/getuserrecipies",myusername,{headers:headers}).map(res=>res.json());
 }
 
 editUser(editData){
@@ -170,7 +170,7 @@ editUser(editData){
   this.fetchtoken();
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/user/updateprofile",editData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/updateprofile",editData,{headers:headers}).map(res=>res.json());
 }
 
 editimage(image){
@@ -179,7 +179,7 @@ editimage(image){
   const formData: FormData = new FormData();
   formData.append('Authorization',token);
   formData.append('editprofpic', image.editimage,image.editimage.name);
-  return this.http.post("http://localhost:9000/user/profpicchange", formData).map(res=>res.json()); 
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/profpicchange", formData).map(res=>res.json()); 
 };
 
 deleterecipe(recipename){
@@ -191,7 +191,7 @@ deleterecipe(recipename){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/deleterecipe",recipeData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/deleterecipe",recipeData,{headers:headers}).map(res=>res.json());
 }
 
 changepassword(password){
@@ -200,7 +200,7 @@ changepassword(password){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/user/changepassword",password,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/changepassword",password,{headers:headers}).map(res=>res.json());
 }
 
 Isadmin(){
@@ -208,7 +208,7 @@ Isadmin(){
   let headers = new Headers();
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/user/isadmin",{sdsd:"sdd"},{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/isadmin",{sdsd:"sdd"},{headers:headers}).map(res=>res.json());
 }
 
 acceptRecipe(recipeData){
@@ -217,13 +217,13 @@ acceptRecipe(recipeData){
   //console.log(this.authtoken);
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/acceptrecipe",recipeData,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/acceptrecipe",recipeData,{headers:headers}).map(res=>res.json());
 }
 
 searchrecipe(recipename){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/foodrecipe/searchrecipe",recipename,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/foodrecipe/searchrecipe",recipename,{headers:headers}).map(res=>res.json());
 }
 
 deleteacc(pass){
@@ -234,13 +234,13 @@ deleteacc(pass){
   let headers = new Headers();
   headers.append('Authorization',this.authtoken);
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/user/deleteuser",password,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/deleteuser",password,{headers:headers}).map(res=>res.json());
 }
 
 fogotpassword(username){
   let headers = new Headers();
   headers.append('content-Type','application/json');
-  return this.http.post("http://localhost:9000/user/fogotpassword",username,{headers:headers}).map(res=>res.json());
+  return this.http.post("http://ec2-13-59-30-146.us-east-2.compute.amazonaws.com/user/fogotpassword",username,{headers:headers}).map(res=>res.json());
 }
 
 
